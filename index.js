fa53c3c659bff472b58d7ae284d0e0d95f62c5bb
JuanPablo <jpabloaj@gmail.com>
2017-01-05 21:51:56 -0300

    websocket server
    * to update t38 points

---
+++ index.js
@@ -0,0 +1,49 @@
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({port:8080});
+
+var utils = require('./lib/utils');
+
+wss.on('connection', function connection(ws) {
+  var name;
+  ws.on('message', function incomming(message) {
+    var data = JSON.parse(message);
+    if (data.message == 'init') {
+      var ship = utils.createShip();
+      console.log("Hello Ship %s", ship.name);
+      name = ship.name;
+      ws.send(JSON.stringify({
+        message: "init", name: ship.name, lat:ship.lat, lng:ship.lng
+      }));
+    }
+    if (data.message == 'set position') {
+      // check if message is JSON
+      name = data.name;
+      var lat = data.lat;
+      var lng = data.lng;
+      if (name && lat && lng)
+        utils.updatePosition(name, lat, lng);
+    }
+    if (data.message == 'scan rocks') {
+      utils.scanCollection('rocks', function(data){
+        ws.send(JSON.stringify({message:"rocks", data:data[1]}));
+      });
+    }
+    if (data.message == 'scan ships') {
+      utils.scanCollection('ships', function(data){
+        ws.send(JSON.stringify({message:"ships", data:data[1]}));
+      });
+    }
+    if (data.message === 'show collitions') {
+      utils.fenceRoamObj("ships", data.name, "rocks", function(data) {
+        if ( data && data !== "OK" && ws.readyState === 1 )
+          ws.send(JSON.stringify({message:"show collitions", data:data}));
+      });
+    }
+  });
+  ws.on('close', function close() {
+    if (name) {
+      utils.shipToRock(name);
+      console.log('Bye bye Ship %s, hello rock %s', name, name);
+    }
+  });
+});
